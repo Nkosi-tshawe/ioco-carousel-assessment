0408f172ef66e96137218e38fc5b0f91927bbaa8
Nkosi R Tshawe <ronaldtshawe@gmail.com>
2021-05-06 15:00:46 +0200

    Breakpoint adjustments

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -24,7 +24,7 @@ export class CarouselComponent implements OnInit, AfterViewInit, OnDestroy {
 
   config = {
     slidesToShow: 1,
-    arrows: true,
+    arrows: false,
     centerMode: true,
     variableWidth: true,
     adaptiveHeight: false,
